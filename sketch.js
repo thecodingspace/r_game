c1e1ab0787dbaf14d49659fdc5d8d053659f86fd
thecodingspace <70912776+thecodingspace@users.noreply.github.com>
2021-01-19 20:14:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,8 +61,8 @@ function setup() {
 
   gameFont = loadFont("GameFont.ttf");
 
-  canW = 490; // * 1.5;
-  canH = 871; //* 1.5;
+  canW = 490* 1.5;
+  canH = 871* 1.5;
   canArea = canH * canW;
 
   carWidth = canW / 10;
